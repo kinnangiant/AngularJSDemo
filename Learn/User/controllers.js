29794a06614561a4843d61cce2a50ccd0d1f6f15
Kinnan Nawaz <kinnan.nawaz@giantgroup.com>
2022-09-28 07:24:05 +0100

    Inital Commit

---
+++ Learn/User/controllers.js
@@ -0,0 +1,22 @@
+﻿user.controller('MainController', ['$scope','$location', function ($scope,$location) {
+
+    $scope.Credentials = {
+        UserName: '',
+        Password: ''
+    };
+
+    $scope.Credentials.UserName = '';
+    $scope.Credentials.Password = '';
+
+    $scope.Login = function () {
+
+        //console.log($scope.Credentials.UserName);
+        //console.log($location.path());
+
+        if (!$scope.Credentials)
+            $location.path('/views/User')
+        else
+            alert("hello")
+    }
+
+}]);
